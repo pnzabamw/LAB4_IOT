ad4889628928ed7b0501e7285ba19e35eea6b9b3
pnzabamw <84463741+pnzabamw@users.noreply.github.com>
2021-12-12 16:45:30 +0000

    Add files via upload

---
+++ wot-server.js
@@ -0,0 +1,17 @@
+
+var httpServer = require('./servers/http.js'); //loading http
+var ledsPlugin = require('./plugins/ledsplugin.js');
+ledsPlugin.start({'simulate': true, 'frequency': 2000});
+
+var pirPlugin = require('./plugins/pirPlugin.js');
+
+pirPlugin.start({'simulate': false, 'frequency': 2000});
+
+
+resources = require('./resources/model.js'); //loading the model
+var server = httpServer.listen(resources.pi.port, function () //start the http server
+{
+console.info ('Your WoT Pi is up and running on port %s',
+resources.pi.port); //invoking the callback
+});
+
